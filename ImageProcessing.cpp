6858c92047a82faffa68dd90f18d1df2baad62fc
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2020-10-06 20:00:46 +0530

    Fixed error

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -2,12 +2,13 @@
 
 int main()
 {
-	Image Img("test.bmp");
+	Image Img("cameraman.bmp");
 	vector<uint8_t> inputImageData = Img.getImgData();
-	vector<uint8_t> imgDataOutput;
+	vector<uint8_t> imgDataOutput = Img.getImgData();
 	InfoHeader InputHeader = Img.getInfoHeader();
-	Img.brightnessUp(inputImageData, imgDataOutput, InputHeader.size, 70);
+	int size = InputHeader.height * InputHeader.width;
+	Img.brightnessUp(inputImageData, imgDataOutput, inputImageData.size(), 70);
 	Image outputImg(Img, imgDataOutput);
-	outputImg.write("bright.bmp");
+	outputImg.write("bright2.bmp");
 	return 0;
 }
