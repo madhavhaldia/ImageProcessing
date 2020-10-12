6b19b78a6af99e310e9b451efc0d379806ddc164
Firebolt0308 <40853394+shivam00308@users.noreply.github.com>
2020-10-12 21:14:37 +0530

    sepia filter working

--- ImgReadWrite.h
+++ ImgReadWrite.h
@@ -2,9 +2,13 @@
 #include<fstream>
 #include<vector>
 #include<iostream>
+#include <mutex>
+#include <stdexcept>
+#include <thread>
+std::mutex mtx_sepia;
 using namespace std;
 /// Using Pragma pack to remove padding
-/// The following is the standard header for BMP 
+/// The following is the standard header for BMP
 /// Attributes have been initialised to default values
 /// Dividing the header into 3 structs (file, info and color)
 
@@ -16,7 +20,7 @@ struct FileHeader
 	uint32_t size{ 0 };	//size of file
 	uint16_t res1{ 0 };	//reserved
 	uint16_t res2{ 0 };	//reserved
-	uint32_t begin{ 0 };	//starting byte of pixel data 
+	uint32_t begin{ 0 };	//starting byte of pixel data
 };
 
 struct InfoHeader
@@ -28,7 +32,7 @@ struct InfoHeader
 	//The following fields are constants for the scope of this program beacuse we are working with uncompressed images
 	uint16_t planeNum{ 1 };
 	uint16_t bitcount{ 0 };
-	uint32_t compressID{ 0 }; //can be changed to 3 in case of compression 
+	uint32_t compressID{ 0 }; //can be changed to 3 in case of compression
 	uint32_t imgSize{ 0 }; //always 0 for uncompressed image
 	int32_t xppm{ 0 };
 	int32_t yppm{ 0 };
@@ -54,7 +58,11 @@ class Image {
 	ColorHeader colorHeader;
 	int rowPadding{ 0 };
 	vector<uint8_t> imgData;
- private:
+	uint32_t global_row_sepia_threads{ 0};
+    std::vector<std::thread> some_threads;
+public:
+	vector<vector<uint8_t>> imgTable;
+private:
 
 	int matchPadding(int rowPadding)
 	{
@@ -101,6 +109,7 @@ public:
 		setInfoHeader(Img.getInfoHeader());
 		setColorHeader(Img.getColorHeader());
 		imgData = imageData;
+		rowPadding = Img.rowPadding;
 	}
 	Image(const char* ImgName)
 	{
@@ -178,7 +187,7 @@ public:
 			}
 
 			fileHeader.size = fileHeader.begin;
-			if (infoHeader.height < 0) // this is a check if the image centre is bottom left or top left. 
+			if (infoHeader.height < 0) // this is a check if the image centre is bottom left or top left.
 			{
 				cout << "Throw Error Origin not compatible";
 			}
@@ -188,6 +197,7 @@ public:
 			if (infoHeader.width % 4 == 0)
 			{
 				imgIn.read((char*)imgData.data(), imgData.size());
+				imgIn.read((char*)imgTable.data(), imgTable.size());
 				fileHeader.size += static_cast<uint32_t>(imgData.size());
 			}
 			else
@@ -247,6 +257,81 @@ public:
 		}
 
 	}
+
+	void sepia() {
+
+
+
+
+        uint32_t num_threads = infoHeader.height;
+        global_row_sepia_threads = 0;
+
+        time_t start, end;
+        time(&start);
+        for (uint32_t i = 0; i < num_threads; i++) {
+            some_threads.push_back(std::thread(&Image::transform_row_to_sepia, this));
+            }
+        for (auto& t : some_threads) {
+            t.join();
+        }
+        some_threads.clear();
+        time(&end);
+        std::cout << difftime(end, start) << " seconds (rgb to sepia - HEIGHT: " << infoHeader.height << " threads, 1 per row, WIDTH: " << infoHeader.width << ")" << std::endl;
+
+    }
+
+    transform_row_to_sepia() {
+
+        uint32_t channels = infoHeader.bitcount / 8;
+        uint32_t A{0}, R{0}, G{0}, B{0}, grey{0};
+        uint32_t tr{0}, tg{0}, tb{0};
+        mtx_sepia.lock();
+        for (uint32_t column = 0; column < infoHeader.width; column++) {
+            R = imgData[channels * (global_row_sepia_threads * infoHeader.width + column) + 0];
+            G = imgData[channels * (global_row_sepia_threads * infoHeader.width + column) + 1];
+            B = imgData[channels * (global_row_sepia_threads * infoHeader.width + column) + 2];
+
+            tr = 0.393 * R + 0.769 * G + 0.189 * B;
+            tg = 0.349 * R + 0.686 * G + 0.168 * B;
+            tb = 0.272 * R + 0.534 * G + 0.131 * B;
+
+            if (tr > 255) R = 255;
+            else R = tr;
+            if (tg > 255) G = 255;
+            else G = tg;
+            if (tb > 255) B = 255;
+            else B = tb;
+
+            imgData[channels * (global_row_sepia_threads * infoHeader.width + column) + 0] = R;
+            imgData[channels * (global_row_sepia_threads * infoHeader.width + column) + 1] = G;
+            imgData[channels * (global_row_sepia_threads * infoHeader.width + column) + 2] = B;
+        }
+        global_row_sepia_threads += 1;
+        mtx_sepia.unlock();
+
+    }
+
+	vector<uint8_t> brightnessUp(vector<uint8_t> _inputImgData, vector<uint8_t> _outputImgData, int imgSize, int brightness) {
+		for (int i = 0; i < imgSize; i++) {
+
+			int temp = _inputImgData[i] + brightness;
+			_outputImgData[i] = (temp > 255) ? 255 : temp;
+
+
+		}
+		return _outputImgData;
+	}
+	vector<uint8_t> brightnessDown(vector<uint8_t> _inputImgData, vector<uint8_t> _outputImgData, int imgSize, int brightness) {
+		for (int i = 0; i < imgSize; i++) {
+
+			int temp = _inputImgData[i] - brightness;
+			_outputImgData[i] = (temp < 0) ? 0 : temp;
+
+
+		}
+		return _outputImgData;
+	}
+
 	FileHeader getFileHeader()
 	{
 		return fileHeader;
@@ -276,4 +361,3 @@ public:
 		colorHeader = data;
 	}
 };
-
